7d984c806a259c60543a9a695859d96fd553e9d3
m1crochip <61010416@kmitl.ac.th>
2020-02-13 14:14:14 +0700

    Updates

---
+++ precache-manifest.26a3f904a416b4b0b6ab7a0fdadf981f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d84d2ed193adba61d388256e17c41f8",
+    "url": "/softdev/index.html"
+  },
+  {
+    "revision": "eb35b2e157fb5731f676",
+    "url": "/softdev/static/css/2.a14ccbaf.chunk.css"
+  },
+  {
+    "revision": "b8a4ef60526be77d66e4",
+    "url": "/softdev/static/css/main.9406a71e.chunk.css"
+  },
+  {
+    "revision": "eb35b2e157fb5731f676",
+    "url": "/softdev/static/js/2.f5777666.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/softdev/static/js/2.f5777666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8a4ef60526be77d66e4",
+    "url": "/softdev/static/js/main.3ec00c15.chunk.js"
+  },
+  {
+    "revision": "7176bafd06ea21e9a587",
+    "url": "/softdev/static/js/runtime-main.6dd7db4d.js"
+  },
+  {
+    "revision": "abc37022736bcc61af3127680be72032",
+    "url": "/softdev/static/media/download.abc37022.jpg"
+  },
+  {
+    "revision": "ff558db71fd9e245302f42459daee004",
+    "url": "/softdev/static/media/download1.ff558db7.jpg"
+  },
+  {
+    "revision": "c7343da5c9f7a39936ffab057efa2316",
+    "url": "/softdev/static/media/download3.c7343da5.jpg"
+  },
+  {
+    "revision": "92ca92bb33b8f319691c04e9a07f167d",
+    "url": "/softdev/static/media/profile.92ca92bb.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/softdev/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/softdev/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/softdev/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/softdev/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "1c9dc1a1856b6b26e113e74596343aea",
+    "url": "/softdev/static/media/weblogo_white.1c9dc1a1.png"
+  }
+]);
